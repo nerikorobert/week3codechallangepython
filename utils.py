c6d964f820f84542a044c47912acf260c2d81403
robert neriko <nerikorobert@gmail.com>
2023-10-04 13:36:25 +0300

    Delete unnecessary files

--- utils.py
+++
@@ -1,7 +0,0 @@
-from models import Customer, Restaurant, Review
-
-def clear_db(session):
-    session.query(Customer).delete()
-    session.query(Restaurant).delete()
-    session.query(Review).delete()
-    session.commit()
